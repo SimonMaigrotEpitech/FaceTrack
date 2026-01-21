fc765fc1ac53399212933e8e679681360c34361d
SimonMaigrot <simon.maigrot@epitech.eu>
2026-01-21 09:43:33 +0100

    feat: Adding rect variables and get functions inside faceDetector class

--- src/main/java/com/facetrack/detection/FaceDetector.java
+++ src/main/java/com/facetrack/detection/FaceDetector.java
@@ -8,9 +8,43 @@ import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 
-public class FaceDetector {
-    private:
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
-    public:
-    
+public class FaceDetector {
+
+    private CascadeClassifier faceClassifier;
+    private boolean loaded = false;
+
+    private Scalar RECT_COLOR = new Scalar(0, 255, 0);
+    private int RECT_THICKNESS = 2;
+    private String CASCADE_FILE = "haarcascade_frontalface_default.xml";
+
+    public FaceDetector()
+    {
+        loadClassifier();
+    }
+
+    private void loadClassifier()
+    {
+        boolean resResources;
+        boolean resEnvPath;
+        boolean resSystemPaths;
+
+        if (resResources || resEnvPath ||  resSystemPaths)
+            return;
+        printErrorMessage();
+    }
+
+    private void printErrorMessage()
+    {
+        System.err.println("Téléchargez le file de Haar (abe) et mettez le dans src/main/resources/");
+    }
+
+    public boolean isLoaded()
+    {
+        return loaded;
+    }
 }
