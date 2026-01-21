0e4d4a34d9cca923dbf9b15c3f3437f2bbbb43e0
SimonMaigrot <simon.maigrot@epitech.eu>
2026-01-21 09:20:16 +0100

    feat: Adding opencv imports

--- src/main/java/com/facetrack/detection/FaceDetector.java
+++ src/main/java/com/facetrack/detection/FaceDetector.java
@@ -1,3 +1,13 @@
+package com.facetrack.detection;
+
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
 public class FaceDetector {
     private:
 
