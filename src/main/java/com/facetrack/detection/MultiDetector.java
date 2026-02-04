7471692c4c653536a5c37f540b23fb36b25ecb30
SimonMaigrot <simon.maigrot@epitech.eu>
2026-02-04 18:59:39 +0100

    feat: multi detection class including eye, smile, and profile

---
+++ src/main/java/com/facetrack/detection/MultiDetector.java
@@ -0,0 +1,25 @@
+package com.facetrack.detection;
+
+import org.opencv.objdetect.CascadeClassifier;
+
+public class MultiDetector {
+
+    private CascadeClassifier eyeClassifier;
+    private CascadeClassifier smileClassifier;
+    private CascadeClassifier profileClassifier;
+
+    private boolean eyeEnabled = false;
+    private boolean smileEnabled = false;
+    private boolean profileEnabled = false;
+
+    private int eyeCount = 0;
+    private int smileCount = 0;
+    private int profileCount = 0;
+
+    public MultiDetector()
+    {
+        eyeClassifier = new CascadeClassifier("src/main/resources/haarcascade_eye.xml");
+        smileClassifier = new CascadeClassifier("src/main/resources/haarcascade_smile.xml");
+        profileClassifier = new CascadeClassifier("src/main/resources/haarcascade_profileface.xml");
+    }
+}
